b7cb808446512da8a4aa6a64663f23c60fc95d05
David Naylor <github@devodo.com>
2014-08-19 14:09:20 +0100

    Exclude non js files from routes

--- src/web.api/routes/test.js
+++ src/web.api/routes/test.js
@@ -5,15 +5,18 @@ exports.init = function init(app) {
     app.get('/api/test/error-handling', function (req, res) {
         throw new Error('Deliberately thrown error.'); 
     });
-	
-	app.get('/api/test/ping', function (req, res) {
-        res.json({message: 'pong'});
+
+    app.get('/api/test/ping', function (req, res) {
+        res.json({
+            message: 'pong',
+            date: new Date()
+        });
     });
-	
-	app.get('/api/test/not-found-handling', function (req, res) {
+
+    app.get('/api/test/not-found-handling', function (req, res) {
         var err = new Error('Deliberate Not Found');
         err.status = 404;
         throw err;
     });
-	 
+
 };
