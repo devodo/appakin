057fceb514ce4e9460a5be8da48f523f6eabc9f6
Stephen Johns <steve@stevejay.net>
2014-09-15 17:13:49 +0100

    reworked look to be based around dark grey, gold and blue

--- src/web.public/public/scripts/appakin/configModule.js
+++ src/web.public/public/scripts/appakin/configModule.js
@@ -2,6 +2,6 @@
     'use strict';
 
     angular.module('appAkin.config', [])
-        .constant('webApiUrl', 'http://10.0.1.4:3002/');
+        .constant('webApiUrl', 'http://10.0.1.6:3002/');
 
 }()); // use strict
