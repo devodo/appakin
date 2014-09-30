7587244193a55d1756abaca01d1b2f071a6d362c
Stephen Johns <steve@stevejay.net>
2014-09-30 21:55:24 +0100

    removed platform searchbox code

--- src/web.public/public/scripts/appakin/search/akin-searchbox.js
+++ src/web.public/public/scripts/appakin/search/akin-searchbox.js
@@ -15,43 +15,6 @@
                         search.submitSearch(1);
                     }, 0);
                 };
-
-//                $scope.ddSelectOptions = [
-//                    {
-//                        text: $scope.platform.getStoreName('android'),
-//                        platform: 'android'
-//                    },
-//                    {
-//                        text: $scope.platform.getStoreName('ios'),
-//                        platform: 'ios'
-//                    },
-//                    {
-//                        text: $scope.platform.getStoreName('winphone'),
-//                        platform: 'winphone'
-//                    }
-//                ];
-//
-//                $scope.ddSelectSelected = {
-//                    text: $scope.platform.getStoreName($scope.search.platform),
-//                    platform: $scope.search.platform
-//                };
-//
-//                // TODO: There may be a better way to manage platform updating.
-//                $scope.$watch('search.platform', function(newValue, oldValue) {
-//                    $scope.ddSelectSelected = {
-//                        text: $scope.platform.getStoreName($scope.search.platform),
-//                        platform: $scope.search.platform
-//                    };
-//                });
-//
-//                $scope.dropdownOnchange = function (selected) {
-//                    if ($scope.search.platform === selected.platform) {
-//                        return;
-//                    }
-//
-//                    $scope.search.platform = selected.platform;
-//                    $rootScope.$broadcast('userChangedPlatform');
-//                };
             }
         };
     });
