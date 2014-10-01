b4baf84abc51561eacd692773fefd01f84305f00
Stephen Johns <steve@stevejay.net>
2014-10-01 10:36:39 +0100

    tweaks

--- src/web.public/public/scripts/appakin/appAkinCtrl.js
+++ src/web.public/public/scripts/appakin/appAkinCtrl.js
@@ -1,8 +1,13 @@
 (function () {
     'use strict';
 
-    angular.module('appAkin').controller('AppAkin', function($scope, pageTitle) {
+    angular.module('appAkin').controller('AppAkin', function($scope, pageTitle, $rootScope, loading) {
         $scope.pageTitle = pageTitle;
+
+        $rootScope.$on('$locationChangeSuccess', function() {
+            console.log('spinner reset');
+            loading.reset();
+        });
     });
 
 }()); // use strict
