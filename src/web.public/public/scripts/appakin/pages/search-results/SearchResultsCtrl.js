50f27d462112b0e566e2ba619a2c1c55b81c14a2
Stephen Johns <steve@stevejay.net>
2014-10-02 00:14:27 +0100

    changed how search results page is bound

--- src/web.public/public/scripts/appakin/pages/search-results/SearchResultsCtrl.js
+++ src/web.public/public/scripts/appakin/pages/search-results/SearchResultsCtrl.js
@@ -7,15 +7,24 @@
             $scope.search = search;
             $scope.url = url;
             $scope.numPages = 5;
+            $scope.searchType = search.results.searchType; // could be replaced.
+            $scope.isCategorySearch = search.results.searchType == 'category';
+            $scope.isAppSearch = search.results.searchType == 'app';
+            $scope.serverError = search.results.serverError;
+            $scope.hasResults = search.results.items.length > 0;
+            $scope.suggestion = search.results.suggestion;
+            $scope.resultSearchTerm = search.results.resultSearchTerm;
+            $scope.resultTotalItems = search.results.totalItems;
+            $scope.resultItems = search.results.items;
 
             $scope.setSearchType = function(searchType) {
                 search.searchType = searchType;
                 search.submitSearch(1);
             };
 
-//            $scope.$on('userChangedPlatform', function(event) {
-//                search.submitSearch(1);
-//            });
+            //$scope.$on('userChangedPlatform', function(event) {
+            //    search.submitSearch(1);
+            //});
 
             $scope.pageChanged = function() {
                 if (search.results.resultSearchTerm) {
