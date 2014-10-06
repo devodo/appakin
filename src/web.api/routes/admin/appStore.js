d0b62f9c0859c135c66ede6bb4c6ed551524bde9
David Naylor <github@devodo.com>
2014-10-06 15:40:26 +0100

    Add endpoint for download missing appstore sources

--- src/web.api/routes/admin/appStore.js
+++ src/web.api/routes/admin/appStore.js
@@ -26,6 +26,16 @@ exports.init = function init(app) {
         });
     });
 
+    app.post('/admin/appstore/lookup_missing_sources', function (req, res) {
+        appStoreData.lookupMissingSourceApps(function(err) {
+            if (err) {
+                return res.status(500).json(err);
+            }
+
+            res.json({status: 'success'});
+        });
+    });
+
     app.get('/admin/appstore/insert_missing_xyo_categories', function (req, res) {
         appStoreData.insertMissingXyoCategories(function(err, results) {
             if (err) {
