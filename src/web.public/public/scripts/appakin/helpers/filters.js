2efa267c7cb6bdc3503fa92cd1635d2000e46b04
Stephen Johns <steve@stevejay.net>
2014-10-21 15:07:50 +0100

    added start of category obfuscation script and improved rules removal in app descriptions

--- src/web.public/public/scripts/appakin/helpers/filters.js
+++ src/web.public/public/scripts/appakin/helpers/filters.js
@@ -141,7 +141,7 @@
                 return value;
             }
 
-            return value.replace(/-{3,}|\*{3,}/g, '');
+            return value.replace(/(.)\1{5,}/g, ' ');
         };
     });
 
