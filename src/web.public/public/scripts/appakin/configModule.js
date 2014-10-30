4efe984317b2600b2fc55c5e86b8be0f21e38bd4
Stephen Johns <steve@stevejay.net>
2014-10-30 11:04:29 +0000

    fixed social links rendering problem on about page

--- src/web.public/public/scripts/appakin/configModule.js
+++ src/web.public/public/scripts/appakin/configModule.js
@@ -2,10 +2,9 @@
     'use strict';
 
     angular.module('appAkin.config', [])
-        //.constant('webApiUrl', 'http://10.0.1.6:3002/')
+        .constant('webApiUrl', 'http://10.0.1.6:3002/')
         //.constant('webApiUrl', 'http://10.0.1.4:3002/')
         //.constant('webApiUrl', 'http://192.168.0.17:3002/')
-        .constant('webApiUrl', 'http://192.168.1.177:3002/')
         .constant('cacheApiRequests', true)
         .constant('angularDebugInfo', false)
         .constant('googleAnalyticsTracking', false);
