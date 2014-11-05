9206a042b4d844dd0618796495ae0cfa626467c8
David Naylor <github@devodo.com>
2014-11-05 16:37:16 +0000

    Add desc top and all fields to cluster index

--- src/web.api/domain/search/clusterIndexer.js
+++ src/web.api/domain/search/clusterIndexer.js
@@ -9,6 +9,7 @@ var createSolrDoc = function(app) {
         id : app.extId.replace(/\-/g, ''),
         name: app.name,
         desc: app.description,
+        "desc_top": solrCore.getTopWords(app.description, 200),
         "primary_genre": app.genres[0],
         genres: app.genres
     };
