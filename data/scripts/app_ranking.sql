948b82757715649edb346be0872608f947f3cc4f
David Naylor <github@devodo.com>
2014-12-08 13:42:56 +0000

    Change popularity function to logarithmic decay model

--- data/scripts/app_ranking.sql
+++ data/scripts/app_ranking.sql
@@ -51,42 +51,46 @@ $BODY$
 LANGUAGE plpgsql VOLATILE
 COST 100;
 
-
--- Function: reset_app_popularity()
-
 --x^((5/5)* 0.2 * e^(-0.001x) + 0.2 * (1 - e^(-0.001x)))
 --http://fooplot.com/#W3sidHlwZSI6MCwiZXEiOiJ4XigoNS81KSowLjE1KmVeKC0wLjAwMXgpKzAuMTUqKDEtZV4oLTAuMDAxeCkpKSIsImNvbG9yIjoiI0VCMEUwRSJ9LHsidHlwZSI6MCwiZXEiOiJ4XigoNC81KSowLjE1KmVeKC0wLjAxeCkrMC4xNSooMS1lXigtMC4wMXgpKSkiLCJjb2xvciI6IiM0NUEzMjkifSx7InR5cGUiOjAsImVxIjoieF4oKDMvNSkqMC4xNSplXigtMC4wMXgpKzAuMTUqKDEtZV4oLTAuMDF4KSkpIiwiY29sb3IiOiIjQTkxMEU2In0seyJ0eXBlIjowLCJlcSI6InheKCgyLzUpKjAuMTUqZV4oLTAuMDF4KSswLjE1KigxLWVeKC0wLjAxeCkpKSIsImNvbG9yIjoiIzFFMjRENCJ9LHsidHlwZSI6MCwiZXEiOiJ4XigoMS81KSowLjE1KmV4cCgtMC4wMXgpKzAuMTUqKDEtZXhwKC0wLjAxeCkpKSIsImNvbG9yIjoiI0U4OTQzQSJ9LHsidHlwZSI6MTAwMCwid2luZG93IjpbIi0yIiwiMTAiLCItMiIsIjQiXX1d
 
--- DROP FUNCTION reset_app_popularity();
-
 -- Function: reset_app_popularity()
 
 -- DROP FUNCTION reset_app_popularity();
+
+select reset_app_popularity();
 
 CREATE OR REPLACE FUNCTION reset_app_popularity()
   RETURNS boolean AS
   $BODY$
+DECLARE avg_rating double precision;
+DECLARE avg_rating_current double precision;
 BEGIN
 	delete from app_popularity;
 
 	INSERT INTO app_popularity(app_id, popularity)
 	select rating.app_id, GREATEST(rating_rank, chart_rank) as rank
 	from (
-		select a.app_id, power(rating_rate, ((0.5 * rating/5.0) * exp(-0.01 * rating_rate)) + (0.5 * (1 - exp(-0.01 * rating_rate)))) - 1 as rating_rank
-		from (
-			select a.app_id, (r1 * r1_count_root + r2 * r2_count)/(r1_count_root + r2_count) as rating, rating_rate
+		select a.app_id, score / max_score as rating_rank from
+		(
+			select a.app_id, score, max(score) over() as max_score
 			from (
-				select a.app_id,
-					coalesce(a.user_rating::double precision, 0) as r1,
-					coalesce(power(a.rating_count::double precision, 0.6), 0) as r1_count_root,
-					coalesce(a.user_rating_current::double precision, 0) as r2,
-					coalesce(a.rating_count_current::double precision, 0) as r2_count,
-					1 + (a.rating_count / power(GREATEST(((EXTRACT(EPOCH FROM NOW() at time zone 'utc') - EXTRACT(EPOCH FROM release_date)) / 86400), 10), 0.5)) as rating_rate
-				from appstore_app a
-				where a.rating_count is not null
+				select a.app_id, ln(1 + (20 * GREATEST(rating_count * rating / 5.0, 1) )/age_days) as score from
+				(
+					select a.app_id, (r1 * r1_count_root + r2 * r2_count)/(GREATEST(1, r1_count_root + r2_count)) as rating, rating_count, age_days
+					from (
+						select app_id,
+							coalesce(user_rating::double precision, 0) as r1,
+							coalesce(power(rating_count::double precision, 0.6), 0) as r1_count_root,
+							coalesce(rating_count::double precision, 0) as rating_count,
+							coalesce(user_rating_current::double precision, 0) as r2,
+							coalesce(rating_count_current::double precision, 0) as r2_count,
+							GREATEST(((EXTRACT(EPOCH FROM NOW() at time zone 'utc') - EXTRACT(EPOCH FROM release_date)) / 86400), 10) as age_days
+						from appstore_app
+					) a
+				) a
 			) a
 		) a
-		where a.rating > 0
 	) rating
 	left join (
 		select a.app_id, 1 - (power(log(min(chart.position))/log(10000),2)) as chart_rank
@@ -102,6 +106,7 @@ END;
 $BODY$
 LANGUAGE plpgsql VOLATILE
 COST 100;
+
 
 
 
