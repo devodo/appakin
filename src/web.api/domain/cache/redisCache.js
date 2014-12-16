029530106cdfabb51228a0b35148f274070f0eee
David Naylor <github@devodo.com>
2014-12-16 12:08:14 +0000

    Add redis framework

---
+++ src/web.api/domain/cache/redisCache.js
@@ -0,0 +1,285 @@
+'use strict';
+
+var redis = require("redis");
+var config = require('../../config');
+var log = require('../../logger');
+var redisCacheMap = Object.create(null);
+
+var RedisCache = function(db) {
+    this.db = db;
+    this.client = null;
+    this.enabled = config.cache.redis.enabled;
+};
+
+RedisCache.prototype.onError = function(err) {
+    var self = this;
+    log.error(err, "Redis error occured on db: " + self.db);
+    self.enabled = false;
+    self.client.end();
+    self.client = null;
+
+    setTimeout(function() {
+        self.createClient(function(err) {
+            if (err) {
+                log.error(err, "Redis error reconnecting to db: " + self.db);
+                // self.createClient should register error handler again for retry loop so can just return here
+                return;
+            }
+
+            self.enabled = true;
+        });
+    }, config.cache.redis.retryConnectionMs);
+};
+
+RedisCache.prototype.createClient = function(next) {
+    var self = this;
+    log.info("Opening Redis connection to db: " + self.db);
+
+    self.client = redis.createClient(config.cache.redis.port, config.cache.redis.host);
+    self.client.on("error", self.onError);
+    self.client.select(self.db, function(err) {
+        if (err) { return next(err); }
+
+        return next(null, self.client);
+    });
+};
+
+RedisCache.prototype.getClient = function(next) {
+    var self = this;
+    if (!self.enabled) {
+        return next();
+    }
+
+    if (self.client !== null) {
+        next(null, self.client);
+    } else {
+        self.createClient(next);
+    }
+};
+
+RedisCache.prototype.get = function(key, next) {
+    var self = this;
+
+    self.getClient(function(err, client) {
+        if (err) { return next(err); }
+
+        if (!client) { return next(null, null); }
+
+        client.get(key, function(err, res) {
+            if (err) { return next(err); }
+
+            return next(null, res);
+        });
+    });
+};
+
+RedisCache.prototype.mget = function(keys, next) {
+    var self = this;
+
+    self.getClient(function(err, client) {
+        if (err) { return next(err); }
+
+        if (!client) { return next(null, null); }
+
+        client.mget(keys, function(err, res) {
+            if (err) { return next(err); }
+
+            return next(null, res);
+        });
+    });
+};
+
+RedisCache.prototype.getObject = function(key, next) {
+    var self = this;
+    self.get(key, function(err, res) {
+        if (err) { return next(err); }
+
+        if (res === null) {
+            return next(null, null);
+        }
+
+        try {
+            var result = JSON.parse(res);
+            return next(null, result);
+        } catch (ex) {
+            log.error(ex, 'Error parsing redis string on key: %s in db: %d', key, self.db);
+            return next(ex);
+        }
+    });
+};
+
+RedisCache.prototype.getObjects = function(keys, next) {
+    var self = this;
+    self.mget(keys, function(err, res) {
+        if (err) { return next(err); }
+
+        if (res === null) {
+            return next(null, null);
+        }
+
+        var results = [];
+
+        try {
+            res.forEach(function(reply) {
+                var result = JSON.parse(reply);
+                results.push(result);
+            });
+
+            return next(null, results);
+        } catch (ex) {
+            log.error(ex, 'Error parsing redis string on key: %s in db: %d', key, self.db);
+            return next(ex);
+        }
+    });
+};
+
+RedisCache.prototype.setEx = function(key, value, expirySeconds, next) {
+    var self = this;
+
+    self.getClient(function(err, client) {
+        if (err) { return next(err); }
+
+        if (!client) { return next(); }
+
+        var val = typeof value === 'object' ? JSON.stringify(value) : value;
+        var args = [key, val];
+
+        if (expirySeconds) {
+            args.push('EX');
+            args.push(expirySeconds);
+        }
+
+        client.set(args, function(err, res) {
+            if (err) { return next(err); }
+
+            next(null, res);
+        });
+    });
+};
+
+RedisCache.prototype.msetEx = function(keyValuePairs, expirySeconds, next) {
+    var self = this;
+
+    self.getClient(function(err, client) {
+        if (err) { return next(err); }
+
+        if (!client) { return next(); }
+
+        var multi = client.multi();
+
+        keyValuePairs.forEach(function(keyValuePair) {
+            var val = typeof keyValuePair.value === 'object' ? JSON.stringify(keyValuePair.value) : keyValuePair.value;
+            var args = [keyValuePair.key, val];
+
+            if (expirySeconds) {
+                args.push('EX');
+                args.push(expirySeconds);
+            }
+
+            multi.set(args);
+        });
+
+        // drains multi queue and runs atomically
+        multi.exec(function (err, replies) {
+            if (err) { return next(err); }
+
+            next(null, replies);
+        });
+    });
+};
+
+RedisCache.prototype.set = function(key, obj, next) {
+    this.setEx(key, obj, null, next);
+};
+
+RedisCache.prototype.mset = function(keyValuePairs, next) {
+    this.msetEx(keyValuePairs, null, next);
+};
+
+RedisCache.prototype.deleteKey = function(key, next) {
+    var self = this;
+
+    self.getClient(function(err, client) {
+        if (err) { return next(err); }
+
+        if (!client) { return next(); }
+
+        client.del(key, function(err, res) {
+            if (err) { return next(err); }
+
+            next(null, res);
+        });
+    });
+};
+
+RedisCache.prototype.deleteKeys = function(keys, next) {
+    var self = this;
+
+    self.getClient(function(err, client) {
+        if (err) { return next(err); }
+
+        if (!client) { return next(); }
+
+        var multi = client.multi();
+
+        keys.forEach(function(key) {
+            multi.del(key);
+        });
+
+        // drains multi queue and runs atomically
+        multi.exec(function (err, replies) {
+            if (err) { return next(err); }
+
+            next(null, replies);
+        });
+    });
+};
+
+RedisCache.prototype.end = function(next) {
+    var self = this;
+    self.getClient(function(err, client) {
+        if (err) { return next(err); }
+
+        if (!client) { return next(); }
+
+        delete redisCacheMap[self.db];
+        client.end();
+        next();
+    });
+};
+
+exports.createRedisCache = function(db) {
+    if (!db || isNaN(db)) {
+        throw "Invalid redis db: " + db;
+    }
+    var redisCache = redisCacheMap[db];
+
+    if (!redisCache) {
+        redisCache = new RedisCache(db);
+        redisCacheMap[db] = redisCache;
+    }
+
+    return redisCache;
+};
+
+exports.dbPartitions = {
+    test: 15,
+    category: 0,
+    search: 4
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/web.api/test/domain/cache/testRedisCache.js
@@ -0,0 +1,124 @@
+"use strict";
+
+var redis = require("redis");
+var redisCacheFactory = require("../../../domain/cache/redisCache");
+
+var redisCache;
+
+exports.group = {
+    setUp: function (callback) {
+        redisCache = redisCacheFactory.createRedisCache(redisCacheFactory.dbPartitions.test);
+        redisCache.getClient(function(err, client) {
+            if (err) { return callback(err); }
+
+            client.flushdb(function(err) {
+                callback(err);
+            });
+        });
+    },
+
+    tearDown: function (callback) {
+        redisCache.end(function(err) {
+            callback(err);
+        });
+    },
+
+    testSetString: function (test) {
+        var testkey = "test1";
+        var testString = new Date().toString();
+        redisCache.set(testkey, testString, function(setErr, setReply) {
+            redisCache.getClient(function(err, client) {
+                client.get(testkey, function(err, result) {
+                    test.expect(4);
+                    test.ok(setErr === null, setErr);
+                    test.ok(setReply, "No set reply");
+                    test.ok(err === null, err);
+                    test.equal(result, testString);
+                    test.done();
+                });
+            });
+        });
+    },
+
+    testSetObject: function (test) {
+        var testkey = "test2";
+        var testObj = {
+            name: "test obj",
+            val: new Date().toString()
+        };
+        redisCache.set(testkey, testObj, function(setErr, setReply) {
+            redisCache.getClient(function(err, client) {
+                client.get(testkey, function(err, result) {
+                    test.expect(4);
+                    test.ok(setErr === null, setErr);
+                    test.ok(setReply, "No set reply");
+                    test.ok(err === null, err);
+                    test.deepEqual(JSON.parse(result), testObj);
+                    test.done();
+                });
+            });
+        });
+    },
+
+    testMultiSetString: function (test) {
+        var testData = [];
+        for (var i = 0; i < 10; i++) {
+            testData.push({
+                key: "mtest" + i,
+                value: "mval" + i
+            });
+        }
+        var keys = testData.map(function(item) {
+            return item.key;
+        });
+        var values = testData.map(function(item) {
+            return item.value;
+        });
+
+        redisCache.mset(testData, function(setErr, setReply) {
+            redisCache.getClient(function(err, client) {
+
+                client.mget(keys, function(err, results) {
+                    test.expect(3 + testData.length);
+                    test.ok(setErr === null, setErr);
+                    test.ok(setReply, "No set reply");
+                    test.ok(err === null, err);
+                    results.forEach(function(result, i) {
+                        test.equal(result, values[i]);
+                    });
+                    test.done();
+                });
+            });
+        });
+    },
+
+    testMultiSetGetObject: function (test) {
+        var testData = [];
+        for (var i = 0; i < 10; i++) {
+            testData.push({
+                key: "mtest" + i,
+                value: { test: "mval" + i }
+            });
+        }
+        var keys = testData.map(function(item) {
+            return item.key;
+        });
+        var values = testData.map(function(item) {
+            return item.value;
+        });
+
+        redisCache.mset(testData, function(setErr, setReply) {
+            redisCache.getObjects(keys, function(getErr, results) {
+                test.expect(4 + testData.length);
+                test.ok(setErr === null, setErr);
+                test.ok(getErr === null, getErr);
+                test.ok(setReply, "No set reply");
+                test.ok(results, "No get results");
+                results.forEach(function(result, i) {
+                    test.deepEqual(result, values[i]);
+                });
+                test.done();
+            });
+        });
+    }
+};
