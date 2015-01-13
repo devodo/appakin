ef0a1531436c8728c8190e12349eb0517808eccf
David Naylor <github@devodo.com>
2015-01-13 11:32:47 +0000

    Allow port argument override

--- src/web.api/server.js
+++ src/web.api/server.js
@@ -1,10 +1,12 @@
 'use strict';
 
+var argv = require('yargs').argv;
 var config = require('./config');
 var log = require('./logger');
 var app = require('./app');
 
-var port = config.server.port;
+
+var port = argv.port ? argv.port : config.server.port;
 app.set('port', port);
 
 var server = app.listen(app.get('port'), function() {
