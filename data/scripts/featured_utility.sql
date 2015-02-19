16655ab93f6398089ae06679f373aead72b0aa98
David Naylor <github@devodo.com>
2015-02-19 11:30:01 +0000

    Update sql utils for featured apps

--- data/scripts/featured_utility.sql
+++ data/scripts/featured_utility.sql
@@ -84,6 +84,29 @@ limit 50;
 
 commit;
 
+-- Insert single category
+INSERT INTO featured_category_app(app_id, category_id, weight, featured_app_type, date_created, date_modified)
+select app_id, category_id, popularity, 1, NOW() at time zone 'utc', NOW() at time zone 'utc'
+from (
+	select category_id, app_id, popularity, num from (
+		select category_id, app_id, popularity, row_number() over(partition by category_id order by popularity desc) as num
+		from (
+			select ca.category_id, ca.app_id, ap.popularity
+			from appstore_app a
+			join app_popularity ap on a.app_id = ap.app_id
+			join category_app ca on a.app_id = ca.app_id
+			where a.rating_count is not null
+			and a.release_date > '2013-01-01'
+			and a.date_deleted is null
+			order by ap.popularity desc
+			limit 5000
+		) t
+	) t
+	where num <= 20
+	and category_id = 866
+	order by category_id, popularity desc
+) t;
+
 
 INSERT INTO featured_category_app(app_id, category_id, weight, fixed_order, featured_app_type,
 																	date_created, date_modified, date_deleted, start_date, end_date)
