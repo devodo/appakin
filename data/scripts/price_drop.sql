0072e2e37e7f9a6de5f0022567a880ad33d0a59a
David Naylor <github@devodo.com>
2015-05-18 10:42:47 +0100

    Price drop query

---
+++ data/scripts/price_drop.sql
@@ -0,0 +1,26 @@
+WITH price AS (
+    select *
+    from (
+           select ph.app_id, ph.price, ph.date_created, row_number() OVER (PARTITION BY ph.app_id ORDER BY ph.id DESC) as pos
+           from appstore_price_history ph
+             join category_app ca on ph.app_id = ca.app_id
+           where ph.country_code = 'USA'
+                 and ca.category_id = 765
+         ) t
+    where t.pos <= 2
+)
+select p1.app_id, p1.price, p2.price as old_price, p1.date_created, ap.popularity
+from (
+       select *
+       from price
+       where pos = 1
+             and (date_created + INTERVAL '1 month 3 days') > now() at time zone 'utc'
+     ) p1
+  join (
+         select *
+         from price
+         where pos = 2
+       ) p2 on p1.app_id = p2.app_id and p1.price < p2.price
+  join app_popularity ap on p1.app_id = ap.app_id
+--order by p1.date_created desc
+order by ap.popularity desc
