95ef649aa886a884ee4d169dfd28406f71b95afb
David Naylor <github@devodo.com>
2015-07-22 13:46:00 +0100

    Update sql functions to use new app_ranking table

--- data/scripts/functions/reset_featured_apps.sql
+++ data/scripts/functions/reset_featured_apps.sql
@@ -4,54 +4,54 @@
 
 CREATE OR REPLACE FUNCTION reset_featured_apps()
   RETURNS boolean AS
-  $BODY$
+$BODY$
 BEGIN
   delete from featured_homepage_app
 where featured_app_type = 1;
 
 INSERT INTO featured_homepage_app(app_id, category_id, weight, featured_app_type, date_created, date_modified)
-select app_id, category_id, popularity, 1, NOW() at time zone 'utc', NOW() at time zone 'utc'
+select app_id, category_id, ranking, 1, NOW() at time zone 'utc', NOW() at time zone 'utc'
 from (
-	select category_id, app_id, popularity, num from (
-		select category_id, app_id, popularity, row_number() over(partition by category_id order by popularity desc) as num
+	select category_id, app_id, log(ranking) as ranking, num from (
+		select category_id, app_id, ranking, row_number() over(partition by category_id order by ranking desc) as num
 		from (
-			select ca.category_id, ca.app_id, ap.popularity
+			select ca.category_id, ca.app_id, ar.ranking
 			from appstore_app a
-			join app_popularity ap on a.app_id = ap.app_id
+			join app_ranking ar on a.app_id = ar.app_id and ar.country_code = 'USA'
 			join category_app ca on a.app_id = ca.app_id
 			where a.rating_count is not null
-			and a.release_date > '2014-01-01'
+			and a.release_date > '2014-08-01'
 			and a.date_deleted is null
-			order by ap.popularity desc
+			order by ar.ranking desc
 			limit 5000
 		) t
 	) t
 	where num <= 20
-	order by category_id, popularity desc
+	order by category_id, ranking desc
 ) t;
 
 delete from featured_category_app
 where featured_app_type = 1;
 
 INSERT INTO featured_category_app(app_id, category_id, weight, featured_app_type, date_created, date_modified)
-select t.app_id, t.category_id, t.popularity, 1, NOW() at time zone 'utc', NOW() at time zone 'utc'
+select t.app_id, t.category_id, t.ranking, 1, NOW() at time zone 'utc', NOW() at time zone 'utc'
 from (
-	select category_id, app_id, popularity, num from (
-		select category_id, app_id, popularity, row_number() over(partition by category_id order by popularity desc) as num
+	select category_id, app_id, log(ranking) as ranking, num from (
+		select category_id, app_id, ranking, row_number() over(partition by category_id order by ranking desc) as num
 		from (
-			select ca.category_id, ca.app_id, ap.popularity
+			select ca.category_id, ca.app_id, ar.ranking
 			from appstore_app a
-			join app_popularity ap on a.app_id = ap.app_id
+			join app_ranking ar on a.app_id = ar.app_id and ar.country_code = 'USA'
 			join category_app ca on a.app_id = ca.app_id
 			where a.rating_count is not null
-			and a.release_date > '2014-01-01'
+			and a.release_date > '2014-08-01'
 			and a.date_deleted is null
-			order by ap.popularity desc
+			order by ar.ranking desc
 			limit 5000
 		) t
 	) t
 	where num <= 20
-	order by category_id, popularity desc
+	order by category_id, ranking desc
 ) t
 left join featured_category_app fa
 on t.app_id = fa.app_id and t.category_id = fa.category_id
@@ -64,20 +64,20 @@ select c.id, t.score, NOW() at time zone 'utc', NOW() at time zone 'utc'
 from category c
 left join xyo_category_map x on c.id = x.category_id
 join (
-	select category_id, sum(popularity) / 10.0 as score
+	select category_id, sum(log(ranking)) / 10.0 as score
 	from (
-		select category_id, popularity, num
+		select category_id, ranking, num
 		from (
-			select category_id, popularity, row_number() over(partition by category_id order by popularity desc) as num
+			select category_id, ranking, row_number() over(partition by category_id order by ranking desc) as num
 			from (
-				select ca.category_id, ap.popularity
+				select ca.category_id, ar.ranking
 				from appstore_app a
-				join app_popularity ap on a.app_id = ap.app_id
+				join app_ranking ar on a.app_id = ar.app_id and ar.country_code = 'USA'
 				join category_app ca on a.app_id = ca.app_id
 				where a.rating_count is not null
 				and a.release_date > '2014-01-01'
 				and a.date_deleted is null
-				order by ap.popularity desc
+				order by ar.ranking desc
 				limit 5000
 			) t
 		) t
@@ -94,5 +94,5 @@ limit 50;
   RETURN true;
 END;
 $BODY$
-LANGUAGE plpgsql VOLATILE
-COST 100;
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
